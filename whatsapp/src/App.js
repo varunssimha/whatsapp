0437bd4b300ce440f52d6d7e1450401f507911bb
varunssimha <varunssimha.7@gmail.com>
2022-01-12 13:11:47 +0530

    connected pusher to front end

--- whatsapp/src/App.js
+++ whatsapp/src/App.js
@@ -1,21 +1,28 @@
-import React from 'react'
-import './App.css';
-import Chat from './Chat';
-import Sidebar from './Sidebar';
+import React, { useEffect } from "react";
+import "./App.css";
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
+import Pusher from "pusher-js";
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher("68347bddeb42bc38885b", {
+      cluster: "ap2",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="app">
+      <div className="app_body">
+        <Sidebar />
 
-      <div className='app_body'>
-
-      <Sidebar />
-
-      <Chat />
-
+        <Chat />
       </div>
-  
-
     </div>
   );
 }
